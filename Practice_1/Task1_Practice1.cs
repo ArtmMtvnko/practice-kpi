b296ef45b99e1a4134031990625f3e684e4f2a80
Матвієнко Артем <artemm3notegp@gmail.com>
2022-10-21 19:33:03 +0300

    Test commit practice

---
+++ Practice_1/Task1_Practice1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class World
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your rows number: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int s = 0; s < rows - i; s++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+        }
+    }
+    
+}
